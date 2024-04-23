a76329d804ed05a219f7684a306f8df4abb4d28b
Anna Panferova <anyapanf@gmail.com>
2024-04-23 22:43:59 +0600

    db added and connected

--- src/documents/documentsController.ts
+++ src/documents/documentsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import multer from "multer";
+import { documents } from './documentsRouter';
+import { error } from 'console';
 const sqlite3 = require('sqlite3').verbose();
 
 //multer
@@ -12,33 +14,26 @@ const storage = multer.diskStorage({
     }
 })
 
-//data base
-// const db = new sqlite3.Database('../all_documents.db', sqlite3.OPEN_READWRITE, (err) => {
-//     if (err) {
-//         return console.error(err.message);
-//     }
-//     console.log('Connected to the all_documents SQlite database.');
-// });
-// db.close((err) => {
-//     if (err) {
-//         return console.error(err.message);
-//     }
-//     console.log('Close the database connection.');
-// });
-
 export const upload = multer({ storage: storage })
 
 export const testPostDocument = (req: Request, res: Response) => {
     try {
-        const db = new sqlite3.Database('../uploadedFiles.db');
+        const db = new sqlite3.Database('./uploadedFiles.db');
         console.log(req.file);
-        const query = `INSERT INTO documents (name) VALUES ('${req.file?.filename}')`;
+        const query = `INSERT INTO documents(name) VALUES ('${req.file?.filename}')`;
         db.run(query);
-        console.log(db);
-        
+
+        const sql = `SELECT * FROM documents`
+        db.all(sql, [], (err: { message: any; }, rows: any[]) => {
+            if (err) return console.error(err.message);
+            rows.forEach((row: any) => {
+                console.log(row);
+            })
+        })
         db.close();
         res.status(200).send('Your file is successfully uploaded!');
     } catch (error) {
         res.status(404).send(`Oops! There is something wrong: ${error}`);
     }
 };
+
