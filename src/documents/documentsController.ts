3038e7e61bf2c5b701b6a7427f42c3bf847112f8
Anna Panferova <anyapanf@gmail.com>
2024-05-02 13:08:20 +0600

    new get request added

--- src/documents/documentsController.ts
+++ src/documents/documentsController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express'
 import multer from "multer";
-import { documents } from './documentsRouter';
-import { error } from 'console';
 const sqlite3 = require('sqlite3').verbose();
 
 //multer
@@ -19,21 +17,30 @@ export const upload = multer({ storage: storage })
 export const testPostDocument = (req: Request, res: Response) => {
     try {
         const db = new sqlite3.Database('./uploadedFiles.db');
-        console.log(req.file);
         const query = `INSERT INTO documents(name) VALUES ('${req.file?.filename}')`;
         db.run(query);
-
-        const sql = `SELECT * FROM documents`
-        db.all(sql, [], (err: { message: any; }, rows: any[]) => {
-            if (err) return console.error(err.message);
-            rows.forEach((row: any) => {
-                console.log(row);
-            })
-        })
         db.close();
         res.status(200).send('Your file is successfully uploaded!');
     } catch (error) {
         res.status(404).send(`Oops! There is something wrong: ${error}`);
     }
+};
+
+export const getAllFiles = (req: Request, res: Response) => {
+    const db = new sqlite3.Database('./uploadedFiles.db');
+    const sql = `SELECT DISTINCT id FROM documents
+           ORDER BY created_at DESC`;
+
+    const documentsArr: {
+        id: number, name: string, created_at: number
+    }[] = []
+
+    db.all(sql, [], (err: string, rows: { id: number; name: string; created_at: number; }[]) => {
+        rows.forEach((row: {id: number, name: string, created_at: number}) => {
+            documentsArr.push(row)
+        });
+    });
+    db.close();
+    res.status(200).send(documentsArr);
 };
 
--- src/documents/documentsRouter.ts
+++ src/documents/documentsRouter.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from 'express'
 import { Router } from 'express';
-import { upload, testPostDocument } from './documentsController';
+import { upload, testPostDocument, getAllFiles } from './documentsController';
 import cors from 'cors'
 
 export const documents = Router();
 
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200
 }
 
-documents.get('/', cors(corsOptions), (req: Request, res: Response): void => {
-  res.send("What's up doc ?!");
-});
+// documents.get('/', cors(corsOptions), (req: Request, res: Response): void => {
+//   res.send("What's up doc ?!");
+// });
 
 documents.post("/", upload.single('file'), testPostDocument) 
+
+documents.get('/', cors(corsOptions), getAllFiles);
